920debd3560c7edd75b93428b8e1fba0238739d2
Vishrut Goti <63920621+vishrut9908@users.noreply.github.com>
2024-06-20 16:22:44 -0700

    Projects Updated

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -12,16 +12,8 @@ const Project = () => {
           {projects.map((project, index) => (
             <article className='flex flex-wrap md:justify-between md:items-center' key={index}>
               <picture className={index % 2 === 0 ? 'w-full md:w-6/12 shadow-lg ' : 'w-full md:w-6/12 shadow-lg md:order-1'}>
-                <source
-                  srcSet={`${project.image}-1280.webp 1920w,
-                        ${project.image}-768.webp 768w,
-                        ${project.image}-320.webp 320w`}
-                  sizes='(min-width: 768px) 50vw, 100vw'
-                  type='image/webp'
-                />
-                <source srcSet={`${project.image}-768.jpg 768w,`} sizes='(min-width: 768px) 50vw, 100vw' type='image/jpg' />
 
-                <img width='768px' height='575px' loading='lazy' alt={project.title} className='rounded-lg' />
+                <img src = {project.image} width='768px' height='575px' loading='lazy' alt={project.title} className='rounded-lg' />
               </picture>
               <div className='flex flex-col overflow-auto  space-y-3 my-3 mx-1 w-full md:w-5/12 '>
                 <h3 className='uppercase font-bold text-lg'>{project.title}</h3>
@@ -38,7 +30,7 @@ const Project = () => {
                     <img src='./images/icons/github.svg' alt='link to github page' width='24px' height='24px' />
                   </a>
                   <a href={project.link} target='_blank' rel='noreferrer'>
-                    <img src='./images/icons/external-link.svg' alt='link to live website' width='24px' height='24px' />
+                    {project.link && <img src='./images/icons/external-link.svg' alt='link to live website' width='24px' height='24px' />}
                   </a>
                 </div>
               </div>
